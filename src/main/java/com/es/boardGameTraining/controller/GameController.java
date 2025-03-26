f6bf1c14b5a7c3b4186e5d46f0d0194cffd3e023
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-03-26 16:26:01 +0100

    Create Get All Games Controller

--- src/main/java/com/es/boardGameTraining/controller/GameController.java
+++ src/main/java/com/es/boardGameTraining/controller/GameController.java
@@ -1,14 +1,23 @@
 package com.es.boardGameTraining.controller;
 
+import com.es.boardGameTraining.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.es.boardGameTraining.service.GameService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/games")
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping("/")
+    public List<Game> getGames() {
+        return gameService.getGames();
+    }
 }
