e7767d293a6a351eedb12168f929876c0a12bcc8
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-03-26 16:25:28 +0100

    Create Get All Games function

--- src/main/java/com/es/boardGameTraining/service/GameService.java
+++ src/main/java/com/es/boardGameTraining/service/GameService.java
@@ -3,10 +3,18 @@ package com.es.boardGameTraining.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import com.es.boardGameTraining.model.Game;
+
 import com.es.boardGameTraining.repository.GameRepository;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
+
+    public List<Game> getGames() {
+        return gameRepository.findAll();
+    }
 }
