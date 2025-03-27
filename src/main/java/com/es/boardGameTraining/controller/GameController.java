d06722ff9f9b78c45dd1fac7fe751ff7fd467c5f
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-03-27 09:04:20 +0100

    Update GameController.java

--- src/main/java/com/es/boardGameTraining/controller/GameController.java
+++ src/main/java/com/es/boardGameTraining/controller/GameController.java
@@ -1,10 +1,14 @@
 package com.es.boardGameTraining.controller;
 
+import com.es.boardGameTraining.dto.GameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.es.boardGameTraining.service.GameService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/games")
