88c80862263e76587a3c4519e11af0e954186c3e
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-03-27 09:05:41 +0100

    Update GameService.java

--- src/main/java/com/es/boardGameTraining/service/GameService.java
+++ src/main/java/com/es/boardGameTraining/service/GameService.java
@@ -1,10 +1,13 @@
 package com.es.boardGameTraining.service;
 
+import com.es.boardGameTraining.dto.GameDTO;
 import com.es.boardGameTraining.util.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.es.boardGameTraining.repository.GameRepository;
+
+import java.util.List;
 
 @Service
 public class GameService {
