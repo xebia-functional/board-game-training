b6a709382abb2f5cd5c46ed30159e8fbe2551ea0
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-03-27 12:51:37 +0100

    Deleted

--- src/main/java/com/es/boardGameTraining/util/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.es.boardGameTraining.util.exception;
-
-public class BadRequestException extends RuntimeException {
-    private static final String description = "Bad Request (400)";
-
-    public BadRequestException(String message) {
-        super(description + ". " + message);
-    }
-}
--- src/main/java/com/es/boardGameTraining/util/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.es.boardGameTraining.util.exception;
-
-public class NotFoundException extends RuntimeException {
-    private static final String description = "Not Found (404)";
-
-    public NotFoundException(String message) {
-        super(description + ". " + message);
-    }
-}
