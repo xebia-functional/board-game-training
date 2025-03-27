c4cafbf4f2fce8c163fc0521801594851f5ee1a4
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-03-27 10:54:19 +0100

    Update GameService.java

--- src/main/java/com/es/boardGameTraining/service/GameService.java
+++ src/main/java/com/es/boardGameTraining/service/GameService.java
@@ -1,12 +1,15 @@
 package com.es.boardGameTraining.service;
 
 import com.es.boardGameTraining.dto.GameDTO;
+import com.es.boardGameTraining.model.Game;
 import com.es.boardGameTraining.util.Mapper;
+import com.es.boardGameTraining.util.exception.DataBaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.es.boardGameTraining.repository.GameRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,4 +19,22 @@ public class GameService {
 
     @Autowired
     private Mapper mapper;
+
+    public List<GameDTO> getAllGames() {
+        List<Game> games;
+
+        try {
+            games = gameRepository.findAll();
+        } catch (Exception e) {
+            throw new DataBaseException("database unexpected error" + e.getMessage());
+        }
+
+        List<GameDTO> gameDTOs = new ArrayList<>();
+
+        for (Game game : games) {
+            gameDTOs.add(mapper.entityToDTO(game));
+        }
+
+        return gameDTOs;
+    }
 }
