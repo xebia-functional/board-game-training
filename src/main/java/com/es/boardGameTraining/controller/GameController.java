583504f26f221f3f01f742232100be2d401fc0c0
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-03-27 11:24:55 +0100

    Update GameController.java

--- src/main/java/com/es/boardGameTraining/controller/GameController.java
+++ src/main/java/com/es/boardGameTraining/controller/GameController.java
@@ -1,10 +1,12 @@
 package com.es.boardGameTraining.controller;
 
+import com.es.boardGameTraining.dto.GameBggDTO;
 import com.es.boardGameTraining.dto.GameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class GameController {
     @GetMapping("/")
     public ResponseEntity<List<GameDTO>> getAllGames() {
         return new ResponseEntity<>(gameService.getAllGames(), HttpStatus.OK);
+    }
+
+    @GetMapping("/search/{name}")
+    public ResponseEntity<List<GameBggDTO>> searchGames(@PathVariable String name) {
+        return new ResponseEntity<>(gameService.searchGames(name), HttpStatus.OK);
     }
 }
