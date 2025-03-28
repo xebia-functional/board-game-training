53a6be29edf1caca11b617af0ea412682259b7cf
Gacha <jlgacha75@gmail.com>
2025-03-28 15:07:11 +0100

    Update PlayController.java

--- src/main/java/com/es/boardGameTraining/controller/PlayController.java
+++ src/main/java/com/es/boardGameTraining/controller/PlayController.java
@@ -1,5 +1,6 @@
 package com.es.boardGameTraining.controller;
 
+import com.es.boardGameTraining.dto.PlayCreateDTO;
 import com.es.boardGameTraining.dto.PlayDTO;
 import com.es.boardGameTraining.service.PlayService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,10 @@ public class PlayController {
     @GetMapping("/")
     public ResponseEntity<List<PlayDTO>> getAllPlays() {
         return new ResponseEntity<>(playService.getAllPlays(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<PlayDTO> createPlay(@RequestBody PlayCreateDTO playCreateDTO) {
+        return new ResponseEntity<>(playService.createPlay(playCreateDTO), HttpStatus.OK);
     }
 }
