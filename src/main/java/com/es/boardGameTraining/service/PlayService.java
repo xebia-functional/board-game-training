21782fb6adcef209fe200422119bad5c129702e8
Gacha <jlgacha75@gmail.com>
2025-03-28 15:07:01 +0100

    Update PlayService.java

--- src/main/java/com/es/boardGameTraining/service/PlayService.java
+++ src/main/java/com/es/boardGameTraining/service/PlayService.java
@@ -1,9 +1,11 @@
 package com.es.boardGameTraining.service;
 
+import com.es.boardGameTraining.dto.PlayCreateDTO;
 import com.es.boardGameTraining.repository.PlayRepository;
 import com.es.boardGameTraining.util.Mapper;
 import com.es.boardGameTraining.dto.PlayDTO;
 import com.es.boardGameTraining.model.Play;
+import com.es.boardGameTraining.util.exception.BadRequestException;
 import com.es.boardGameTraining.util.exception.DataBaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,5 +37,51 @@ public class PlayService {
         }
 
         return playDTOs;
+    }
+
+    public PlayDTO createPlay(PlayCreateDTO playCreateDTO) {
+        validatePlayCreateDTO(playCreateDTO);
+
+        Play play = mapper.dtoToEntity(playCreateDTO);
+
+        try {
+            playRepository.save(play);
+        } catch (Exception e) {
+            throw new DataBaseException("Database unexpected error: " + e.getMessage());
+        }
+
+        return mapper.entityToDTO(play);
+    }
+
+    private void validatePlayCreateDTO(PlayCreateDTO playCreateDTO) {
+        if (playCreateDTO == null) {
+            throw new BadRequestException("Body cannot be null");
+        }
+
+        if (playCreateDTO.getLocation() == null || playCreateDTO.getLocation().isBlank()) {
+            throw new BadRequestException("Location cannot be null");
+        }
+
+        if (playCreateDTO.getPlayers() == null || playCreateDTO.getPlayers().isEmpty()) {
+            throw new BadRequestException("Players cannot be empty");
+        }
+
+        if (playCreateDTO.getGame() == null) {
+            throw new BadRequestException("Game cannot be null");
+        }
+
+        if (playCreateDTO.getWinner() != null) {
+            if (playCreateDTO.getWinner() == 0L) {
+                throw new BadRequestException("Incorrect winner id");
+            }
+
+            if (playCreateDTO.getWinner() < 0L) {
+                throw new BadRequestException("Winner id cannot be negative");
+            }
+
+            if (!playCreateDTO.getPlayers().contains(playCreateDTO.getWinner())) {
+                throw new BadRequestException("Incorrect winner id. Winner id must be one of the players");
+            }
+        }
     }
 }
