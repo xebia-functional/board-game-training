2b7f183fbb26da5a9cf07ce48be3f7df3622402a
Gacha <jlgacha75@gmail.com>
2025-03-28 10:19:42 +0100

    Update ParseXmlResponse.java

--- src/main/java/com/es/boardGameTraining/util/ParseXmlResponse.java
+++ src/main/java/com/es/boardGameTraining/util/ParseXmlResponse.java
@@ -1,7 +1,7 @@
 package com.es.boardGameTraining.util;
 
 import com.es.boardGameTraining.dto.GameBggDTO;
-import com.es.boardGameTraining.dto.GameDTO;
+import com.es.boardGameTraining.model.Game;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
@@ -13,7 +13,7 @@ import org.xml.sax.InputSource;
 
 @Service
 public class ParseXmlResponse {
-    public List<GameBggDTO> parseXmlResponseGame(String xml) throws Exception {
+    public List<GameBggDTO> parseXmlResponseGameBGG(String xml) throws Exception {
         List<GameBggDTO> games = new ArrayList<>();
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -27,25 +27,22 @@ public class ParseXmlResponse {
         for (int i = 0; i < items.getLength(); i++) {
             Element item = (Element) items.item(i);
             String id = item.getAttribute("id");
-            String title = ((Element) item.getElementsByTagName("name").item(0)).getAttribute("value");
+            String title = ((Element) item.getElementsByTagName("name").item(0)).getAttribute("value");;
 
-            NodeList yearNodes = item.getElementsByTagName("yearpublished");
-            String year = (yearNodes.getLength() > 0) ? ((Element) yearNodes.item(0)).getAttribute("value") : null;
+            Integer year = extractIntegerValue(item, "yearpublished");
 
             GameBggDTO game = new GameBggDTO();
             game.setId(Long.valueOf(id));
             game.setName(title);
-
-            game.setYearPublished(year != null ? Integer.valueOf(year) : null);
+            game.setYearPublished(year);
 
             games.add(game);
         }
 
-
         return games;
     }
 
-    public GameDTO parseXmlResponseGameDto(String xml) throws Exception {
+    public Game parseXmlResponseGame(String xml) throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse(new InputSource(new StringReader(xml)));
@@ -56,21 +53,69 @@ public class ParseXmlResponse {
 
         for (int i = 0; i < items.getLength(); i++) {
             Element item = (Element) items.item(i);
-            String id = item.getAttribute("id");
+            Long bggId = Long.valueOf(item.getAttribute("id"));
             String title = ((Element) item.getElementsByTagName("name").item(0)).getAttribute("value");
 
-            NodeList yearNodes = item.getElementsByTagName("yearpublished");
-            String year = (yearNodes.getLength() > 0) ? ((Element) yearNodes.item(0)).getAttribute("value") : null;
+            Integer year = extractIntegerValue(item, "yearpublished");
+            Integer minPlayers = extractIntegerValue(item, "minplayers");
+            Integer maxPlayers = extractIntegerValue(item, "maxplayers");
+            Integer age = extractIntegerValue(item, "minage");
+            Integer minPlayTime = extractIntegerValue(item, "minplaytime");
+            Integer maxPlayTime = extractIntegerValue(item, "maxplaytime");
 
-            GameDTO game = new GameDTO();
-            game.setId(Long.valueOf(id));
+            String urlImage = extractStringValue(item, "image");
+            String urlThumbnail = extractStringValue(item, "thumbnail");
+
+            List<String> authors = extractMultipleValues(item, "boardgamedesigner");
+            List<String> artists = extractMultipleValues(item, "boardgameartist");
+
+            Game game = new Game();
+            game.setBggId(bggId);
             game.setTitle(title);
-
-            game.setYear(year != null ? Integer.valueOf(year) : null);
+            game.setYear(year);
+            game.setMinPlayers(minPlayers);
+            game.setMaxPlayers(maxPlayers);
+            game.setAge(age);
+            game.setMinPlayTime(minPlayTime);
+            game.setMaxPlayTime(maxPlayTime);
+            game.setUrlImage(urlImage);
+            game.setUrlThumbnail(urlThumbnail);
+            game.setAuthors(authors);
+            game.setArtists(artists);
+            game.setType(item.getAttribute("type"));
 
             return game;
         }
 
         return null;
     }
+
+    private Integer extractIntegerValue(Element item, String tagName) {
+        NodeList nodes = item.getElementsByTagName(tagName);
+        if (nodes.getLength() > 0) {
+            return Integer.valueOf(((Element) nodes.item(0)).getAttribute("value"));
+        }
+        return null;
+    }
+
+    private String extractStringValue(Element item, String tagName) {
+        NodeList nodes = item.getElementsByTagName(tagName);
+        if (nodes.getLength() > 0) {
+            return nodes.item(0).getTextContent();
+        }
+        return null;
+    }
+
+    private List<String> extractMultipleValues(Element item, String type) {
+        NodeList nodes = item.getElementsByTagName("link");
+        List<String> values = new ArrayList<>();
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Element element = (Element) nodes.item(i);
+            if (element.getAttribute("type").equals(type)) {
+                values.add(element.getAttribute("value"));
+            }
+        }
+        return values;
+    }
+
 }
