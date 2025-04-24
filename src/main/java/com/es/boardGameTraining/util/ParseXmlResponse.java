9d61847f5b278cdc287c0e959604c48a34a40ff5
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-24 09:50:38 +0200

    Update ParseXmlResponse.java

--- src/main/java/com/es/boardGameTraining/util/ParseXmlResponse.java
+++ src/main/java/com/es/boardGameTraining/util/ParseXmlResponse.java
@@ -13,35 +13,6 @@ import org.xml.sax.InputSource;
 
 @Service
 public class ParseXmlResponse {
-    public List<GameBggDTO> parseXmlResponseGameBGG(String xml) throws Exception {
-        List<GameBggDTO> games = new ArrayList<>();
-
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(new InputSource(new StringReader(xml)));
-
-        XPathFactory xPathFactory = XPathFactory.newInstance();
-        XPath xpath = xPathFactory.newXPath();
-        NodeList items = (NodeList) xpath.evaluate("/items/item", document, XPathConstants.NODESET);
-
-        for (int i = 0; i < items.getLength(); i++) {
-            Element item = (Element) items.item(i);
-            String id = item.getAttribute("id");
-            String title = ((Element) item.getElementsByTagName("name").item(0)).getAttribute("value");;
-
-            Integer year = extractIntegerValue(item, "yearpublished");
-
-            GameBggDTO game = new GameBggDTO();
-            game.setId(Long.valueOf(id));
-            game.setName(title);
-            game.setYearPublished(year);
-
-            games.add(game);
-        }
-
-        return games;
-    }
-
     public Game parseXmlResponseGame(String xml) throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
