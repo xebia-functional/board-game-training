3d780ff966ea4b04e92f18f9568a4b5d13ab59d6
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-24 14:12:23 +0200

    Check Players exists

--- src/main/java/com/es/boardGameTraining/service/PlayService.java
+++ src/main/java/com/es/boardGameTraining/service/PlayService.java
@@ -2,15 +2,22 @@ package com.es.boardGameTraining.service;
 
 import com.es.boardGameTraining.dto.PlayCreateDTO;
 import com.es.boardGameTraining.dto.PlayDTO;
+import com.es.boardGameTraining.dto.PlayerDTO;
 import com.es.boardGameTraining.model.Game;
 import com.es.boardGameTraining.model.Play;
+import com.es.boardGameTraining.model.Player;
 import com.es.boardGameTraining.repository.GameRepository;
 import com.es.boardGameTraining.repository.PlayRepository;
+import com.es.boardGameTraining.repository.PlayerRepository;
 import com.es.boardGameTraining.util.Mapper;
 import com.es.boardGameTraining.util.exception.BadRequestException;
 import com.es.boardGameTraining.util.exception.DataBaseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.es.boardGameTraining.util.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +28,9 @@ public class PlayService {
 
     @Autowired
     private PlayRepository playRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
 
     @Autowired
     private GameRepository gameRepository;
@@ -69,6 +79,23 @@ public class PlayService {
         if (playCreateDTO.getPlayers() == null || playCreateDTO.getPlayers().isEmpty()) {
             throw new BadRequestException("Players cannot be empty");
         }
+
+        List<Long> requestedPlayerIds = playCreateDTO.getPlayers();
+
+        List<Player> players = playerRepository.findAllById(requestedPlayerIds);
+
+        Set<Long> existingPlayerIds = players.stream()
+                .map(Player::getId)
+                .collect(Collectors.toSet());
+
+        List<Long> notExistPlayers = requestedPlayerIds.stream()
+                .filter(id -> !existingPlayerIds.contains(id))
+                .toList();
+
+        if (!notExistPlayers.isEmpty()) {
+            throw new NotFoundException("Players not found with IDs: " + notExistPlayers);
+        }
+
 
         if (playCreateDTO.getGame() == null) {
             throw new BadRequestException("Game cannot be null");
